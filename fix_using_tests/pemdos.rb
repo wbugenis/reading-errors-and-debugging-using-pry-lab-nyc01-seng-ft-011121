8bade0da884fc1d83cab3835c81f5b9abcd2b786
William Bugenis <wbugenis@gmail.com>
2020-12-04 00:16:49 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+    snake = "s" * 10 
+    string = snake + string
   end
+  string
 end
